c32bbac04a6b9e946b9ed4b678a044058f79c7d2
Samuel Osei Adu <asamuel355@yahoo.com>
2022-08-19 10:46:31 +0000

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React, { /* useContext, useEffect, useState */ } from "react";
+import React, { useState } from "react";
 import Logo from './images/logo.png'
 import Avatar from './images/avatar.png'
-import {MdShoppingBasket} from 'react-icons/md'
+import {MdShoppingBasket, MdAdd, MdLogout} from 'react-icons/md'
 import {motion} from 'framer-motion'
 import {Link} from 'react-router-dom'
 
@@ -15,6 +15,8 @@ const Header = () => {
     const provider = new GoogleAuthProvider();
 
     const [user, dispatch] = useStateValue();
+
+    const [isMenu, setIsMenu] = useState(false);
 
     const login = async () =>{
         if(!user){
@@ -54,7 +56,7 @@ const Header = () => {
                 <div className='relative flex items-center justify-center'>
                     <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
                     <div className='w-5 h-5 -top-1 -right-1 rounded-full bg-cartNumbg absolute flex items-center justify-center'>
-                        <p className='text-xs text-white font-semibold'>2</p>
+                        <p className='text-xs text-white font-semibold'>2 </p>
                     </div>
                 </div>
 
@@ -62,13 +64,19 @@ const Header = () => {
                     <motion.img 
                         whileTap={{scale : 0.6}} 
                         className='w-8 h-8 cursor-pointer min-w-[40px] min-h-[40px] shadow-sm rounded-full' 
-                        src={user ? user.photoURL : Avatar} 
-                        alt="User Profile" 
+                        src={user ? user.user.photoURL : Avatar} 
+                        alt={user ? user.user.displayName : "Display Photo"} 
                         onClick={login}
                     />
-                    <div className="flex flex-col w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 px-5 py-2">
-                        <p>New Item</p>
-                        <p>Logout</p>
+                    <div className="flex flex-col w-60 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 px-2 py-2">
+                        {
+                            user && user.user.email === "samueloseiboatenglistowell57@gmail.com" && (
+                                <Link to="/createContainer">
+                                    <p className="flex px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdAdd /> New Item</p>
+                                </Link>
+                            )
+                        }
+                        <p className="flex  px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdLogout /> Logout</p>
                     </div>
                 </div>
                 
