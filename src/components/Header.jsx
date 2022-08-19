c7d18a2b08f36b58e516419522a636ac68e7ef68
Samuel Osei Adu <asamuel355@yahoo.com>
2022-08-19 11:01:44 +0000

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
             });
             localStorage.setItem('user', JSON.stringify(providerData[0]));
         }else{
-            alert('User already exist logout')
+            setIsMenu(!isMenu)
         }
         
     }
@@ -68,16 +68,23 @@ const Header = () => {
                         alt={user ? user.user.displayName : "Display Photo"} 
                         onClick={login}
                     />
-                    <div className="flex flex-col w-60 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 px-2 py-2">
-                        {
-                            user && user.user.email === "samueloseiboatenglistowell57@gmail.com" && (
-                                <Link to="/createContainer">
-                                    <p className="flex px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdAdd /> New Item</p>
-                                </Link>
-                            )
-                        }
-                        <p className="flex  px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdLogout /> Logout</p>
-                    </div>
+                    {
+                        isMenu && (
+                            <motion.div 
+                                initial={{opacity : 0, scale : 0.6}} 
+                                animate={{opacity : 1, scale : 1}}
+                                exit={{opacity : 0, scale : 0.8}}  className="flex flex-col w-60 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 px-2 py-2">
+                                {
+                                    user && user.user.email === "samueloseiboatenglistowell57@gmail.com" && (
+                                        <Link to="/createContainer">
+                                            <p className="flex px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdAdd /> New Item</p>
+                                        </Link>
+                                    )
+                                }
+                                <p className="flex  px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor rounded-sm"> <MdLogout /> Logout</p>
+                            </motion.div>
+                        )
+                    }
                 </div>
                 
             </div>
