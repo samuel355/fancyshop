7495a6ebd67f0db9cd352e1f27777951d9ab8988
Samuel Osei Adu <asamuel355@yahoo.com>
2022-09-28 23:33:33 +0000

    scroll

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -19,13 +19,13 @@ const MainContainer = () => {
                     <p className='text-2xl font-semibold  text-headingColor relative before:absolute before:rounded-lg before:w-24 before:h-1 before:-bottom-3 before:left-0 before:bg-gradient-to-tr from-orange-300 to-orange-700 transition-all ease-in-out duration-100'> Our Fresh & Healthy Fruits</p>
                     <div className='hidden md:flex gap-3 items-center'>
                         <motion.div 
-                            onClick={()=> setScrollValue(-250)}
+                            onClick={()=> setScrollValue(-600)}
                             whileTap={{scale : 0.75}}
                             className='w-8 h-8 rounded-2xl bg-orange-300 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg hover:bg-orange-600 flex items-center justify-center'>
                                 <MdChevronLeft className='text-white text-lg' />
                         </motion.div>
                         <motion.div 
-                            onClick={()=> setScrollValue(250)}
+                            onClick={()=> setScrollValue(600)}
                             whileTap={{scale : 0.75}}
                             className='w-8 h-8 rounded-2xl bg-orange-300 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg hover:bg-orange-600 flex items-center justify-center'>
                                 <MdChevronRight className='text-white text-lg' />
@@ -35,7 +35,7 @@ const MainContainer = () => {
                 <RowContainer 
                     scrollValue={scrollValue}
                     flag={true} 
-                    data={foodItems?.filter((n) => n.category ==='fruits')} 
+                    data={foodItems?.filter((n) => n.category ==='drinks')} 
                 />
             </section>
         </div>
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -22,7 +22,7 @@ const RowContainer = ({flag, data, scrollValue}) => {
                 data && data.map((item) => (
                     <div key={item.id} className='w-300 min-w-[300px] md:min-w-[340px] mx-3 md:w-340 h-auto rounded-lg p-2  my-3 bg-cardOverlay shadow-sm backdrop-blur-lg hover:drop-shadow-lg'>
                         <div className='w-full flex items-center justify-between'>
-                            <motion.img whileHover={{scale: 1.2}} className='w-40 h-40 -mt-8 drop-shadow-2xl' src={item.imageURL} alt={item.title} />
+                            <motion.img whileHover={{scale: 1.2}} className='w-40 h-40 -mt-4 drop-shadow-2xl object-contain' src={item?.imageURL} alt={item?.title} />
                             <motion.div 
                                 whileTap={{scale: 0.7}} 
                                 className='w-7 h-7 rounded-full bg-red-500 flex items-center justify-center cursor-pointer hover:shadow-md'>
@@ -30,10 +30,10 @@ const RowContainer = ({flag, data, scrollValue}) => {
                             </motion.div>
                         </div>
                         <div className='w-full flex flex-col items-end justify-end'>
-                            <p className='text-textColor font-semibold text-base md:text-lg'>{item.title}</p>
-                            <p className='mt-1 text-sm text-gray-500'> {item.calories} Calories</p>
+                            <p className='text-textColor font-semibold text-base md:text-lg'>{item?.title}</p>
+                            <p className='mt-1 text-sm text-gray-500'> {item?.calories} Calories</p>
                             <div className='flex items-center gap-8'>
-                                <p className='text-base text-headingColor font-semibold'>$ <span className='text-sm text-red-500'>{item.price}</span></p>
+                                <p className='text-base text-headingColor font-semibold'>$ <span className='text-sm text-red-500'>{item?.price}</span></p>
                             </div>
                         </div>
                     </div> 
