a1e42a767b0eb1af85db159e8676de464c2b29e1
Samuel Osei Adu <asamuel355@yahoo.com>
2022-09-28 22:02:00 +0000

    fetched

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -5,12 +5,12 @@ import {motion} from 'framer-motion'
 const RowContainer = ({flag, data}) => {
     console.log(data)
     return (
-        <div className={`w-full catBack my-10 ${flag ? 'overflow-x-scroll' : 'overflow-x-hidden'} `}>
+        <div className={`w-full flex items-center catBack my-10 ${flag ? 'overflow-x-scroll' : 'overflow-x-hidden'} `}>
             {
                 data && data.map((item) => (
-                    <div key={item.id} className='w-300 ml-3 md:w-340 h-auto rounded-lg p-2  my-12 bg-cardOverlay shadow-sm backdrop-blur-lg hover:drop-shadow-lg'>
+                    <div key={item.id} className='w-300 min-w-[300px] md:min-w-[340px] ml-3 md:w-340 h-auto rounded-lg p-2  my-12 bg-cardOverlay shadow-sm backdrop-blur-lg hover:drop-shadow-lg'>
                         <div className='w-full flex items-center justify-between'>
-                            <motion.img whileHover={{scale: 1.2}} className='w-40 -mt-8 drop-shadow-2xl' src="https://firebasestorage.googleapis.com/v0/b/fancyshop-f9c6f.appspot.com/o/images%2F1664376158548_r5.png?alt=media&token=5242e99c-6a53-4b8c-bea6-3007f8cf097e" alt="" />
+                            <motion.img whileHover={{scale: 1.2}} className='w-40 h-40 -mt-8 drop-shadow-2xl' src={item.imageURL} alt={item.title} />
                             <motion.div 
                                 whileTap={{scale: 0.7}} 
                                 className='w-7 h-7 rounded-full bg-red-500 flex items-center justify-center cursor-pointer hover:shadow-md'>
@@ -18,10 +18,10 @@ const RowContainer = ({flag, data}) => {
                             </motion.div>
                         </div>
                         <div className='w-full flex flex-col items-end justify-end'>
-                            <p className='text-textColor font-semibold text-base md:text-lg'>Chocolate & Vanilla</p>
-                            <p className='mt-1 text-sm text-gray-500'> 5 Calories</p>
+                            <p className='text-textColor font-semibold text-base md:text-lg'>{item.title}</p>
+                            <p className='mt-1 text-sm text-gray-500'> {item.calories} Calories</p>
                             <div className='flex items-center gap-8'>
-                                <p className='text-base text-headingColor font-semibold'>$ <span className='text-sm text-red-500'>5.25</span></p>
+                                <p className='text-base text-headingColor font-semibold'>$ <span className='text-sm text-red-500'>{item.price}</span></p>
                             </div>
                         </div>
                     </div> 
