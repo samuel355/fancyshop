9dc06a547218fe65df3dcb1b6e272f99e2d851bf
Samuel Osei Adu <asamuel355@yahoo.com>
2022-09-29 12:38:59 +0000

    categories

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -24,7 +24,7 @@ const MenuContainer = () => {
             <div className='w-full px-3 flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none'>
                 {
                     foodItems && categories.map((item)=>(
-                        <motion.div whileTap={{scale : 0.75}} onClick={() => setFilter(item.urlParamName)} key={item.id} className={`group ${filter === item.urlParamName ? 'bg-red-600' : 'bg-bgCard'} w-24 min-w-[94px] h-28 cursor-pointer hover:bg-red-600 rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center duration-150 transition-all ease-in-out`}>
+                        <motion.div whileTap={{scale : 0.75}} onClick={() => setFilter(item.urlParamName)} key={item.id} className={`group ${filter === item.urlParamName ? 'bg-red-600' : 'bg-bgCard'} w-24 min-w-[94px] h-28 cursor-pointer hover:bg-red-600 rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center `}>
                             <div className={`w-10 h-10 ${filter === item.urlParamName ? 'bg-white' : 'bg-red-600'} rounded-full group-hover:bg-bgCard flex items-center justify-center hover:bg-red-500`}>
                                 <IoFastFood className={`text-lg  ${filter === item.urlParamName ? 'text-textColor': 'text-red-100'} group-hover:text-textColor`} />
                             </div>
