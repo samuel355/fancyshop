c574b54e65627188c006bff5c9cae147b4690ec8
Samuel Osei Adu <asamuel355@yahoo.com>
2022-10-07 10:41:53 +0000

    updates

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -18,7 +18,7 @@ const CartItemCard = ({ item, setFlag, flag }) => {
   };
 
   const updateQty = (action, id) => {
-    if (action == "add") {
+    if (action === "add") {
       setQty(qty + 1);
       cartItems.map((item) => {
         if (item.id === id) {
@@ -29,7 +29,7 @@ const CartItemCard = ({ item, setFlag, flag }) => {
       cartDispatch();
     } else {
       // initial state value is one so you need to check if 1 then remove it
-      if (qty == 1) {
+      if (qty === 1) {
         items = cartItems.filter((item) => item.id !== id);
         setFlag(flag + 1);
         cartDispatch();
